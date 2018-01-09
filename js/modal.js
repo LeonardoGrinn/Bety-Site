14a95faff78c84f1a22cddf78ba298c163b6772c
LeonardoSolutio <LeoSolutio@gmail.com>
2018-01-09 15:58:03 -0600

    Primera version sitio Cotaa

---
+++ js/modal.js
@@ -0,0 +1,154 @@
+/* --- Modal window detail info --- */
+$(document).ready(function() {
+  $("a.info").click(function() {
+    $(".pop").fadeIn(300);
+  });
+
+  $(".pop > img, .pop").click(function() {
+    $(".pop").fadeOut(300);
+  });
+});
+
+
+/* --- Modal window detail info 2 --- */
+$(document).ready(function() {
+  $("a.info-2").click(function() {
+    $(".pop-2").fadeIn(300);
+  });
+
+  $(".pop-2 > img, .pop-2").click(function() {
+    $(".pop-2").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 3 --- */
+$(document).ready(function() {
+  $("a.info-3").click(function() {
+    $(".pop-3").fadeIn(300);
+  });
+
+  $(".pop-3 > img, .pop-3").click(function() {
+    $(".pop-3").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 4 --- */
+$(document).ready(function() {
+  $("a.info-4").click(function() {
+    $(".pop-4").fadeIn(300);
+  });
+
+  $(".pop-4 > img, .pop-4").click(function() {
+    $(".pop-4").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 5 --- */
+$(document).ready(function() {
+  $("a.info-5").click(function() {
+    $(".pop-5").fadeIn(300);
+  });
+
+  $(".pop-5 > img, .pop-5").click(function() {
+    $(".pop-5").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 6 --- */
+$(document).ready(function() {
+  $("a.info-6").click(function() {
+    $(".pop-6").fadeIn(300);
+  });
+
+  $(".pop-6 > img, .pop-6").click(function() {
+    $(".pop-6").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 7 --- */
+$(document).ready(function() {
+  $("a.info-7").click(function() {
+    $(".pop-7").fadeIn(300);
+  });
+
+  $(".pop-7 > img, .pop-7").click(function() {
+    $(".pop-7").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 8 --- */
+$(document).ready(function() {
+  $("a.info-8").click(function() {
+    $(".pop-8").fadeIn(300);
+  });
+
+  $(".pop-8 > img, .pop-8").click(function() {
+    $(".pop-8").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 9 --- */
+$(document).ready(function() {
+  $("a.info-9").click(function() {
+    $(".pop-9").fadeIn(300);
+  });
+
+  $(".pop-9 > img, .pop-9").click(function() {
+    $(".pop-9").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 10 --- */
+$(document).ready(function() {
+  $("a.info-10").click(function() {
+    $(".pop-10").fadeIn(300);
+  });
+
+  $(".pop-10 > img, .pop-10").click(function() {
+    $(".pop-10").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 11 --- */
+$(document).ready(function() {
+  $("a.info-11").click(function() {
+    $(".pop-11").fadeIn(300);
+  });
+
+  $(".pop-11 > img, .pop-11").click(function() {
+    $(".pop-11").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 12 --- */
+$(document).ready(function() {
+  $("a.info-12").click(function() {
+    $(".pop-12").fadeIn(300);
+  });
+
+  $(".pop-12 > img, .pop-12").click(function() {
+    $(".pop-12").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 13 --- */
+$(document).ready(function() {
+  $("a.info-13").click(function() {
+    $(".pop-13").fadeIn(300);
+  });
+
+  $(".pop-13 > img, .pop-13").click(function() {
+    $(".pop-13").fadeOut(300);
+  });
+});
+
+/* --- Modal window detail info 14 --- */
+$(document).ready(function() {
+  $("a.info-14").click(function() {
+    $(".pop-14").fadeIn(300);
+  });
+
+  $(".pop-14 > img, .pop-14").click(function() {
+    $(".pop-14").fadeOut(300);
+  });
+});
